408f73a3b31dad6258cda663498ecf4412c935da
Prasanth <prasanththegalaxian@gmail.com>
2019-03-31 11:05:09 +0800

    update

---
+++ html/OpenCV-with-Python/temp.py
@@ -0,0 +1,32 @@
+print (1,2,3,4,5)
+
+for i in range(10):
+  print (i)
+  
+def add(a,b):
+  '''
+  input: a,b
+  returns: None
+  
+  print ("Addition:", a+b)
+  '''
+
+add(a,b)
+
+print (1,2,3,4,5)
+
+for i in range(10):
+  print (i) 
+  
+# Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test
+  
+def sub(a,b):
+  '''
+  input: a,b
+  returns: None
+  
+  print ("Subtraction:", aib)
+  '''
+
+sub(a,b)
+
